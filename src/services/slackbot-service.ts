95026c7f9c27992e072f299a52bb51a2e1c82af1
Krystian Sztadhaus <sztadii@gmail.com>
2020-06-16 18:02:52 +0400

    Change returning type of slack-service method

--- src/services/slackbot-service.ts
+++ src/services/slackbot-service.ts
@@ -1,6 +1,4 @@
-import axios, { AxiosPromise } from 'axios'
-
-type PostMessageType = AxiosPromise | void
+import axios from 'axios'
 
 export default class SlackBotService {
   constructor(private webHookURL: string) {
@@ -9,13 +7,13 @@ export default class SlackBotService {
     }
   }
 
-  postMessageToReminderChannel(message: string): PostMessageType {
+  async postMessageToReminderChannel(message: string): Promise<void> {
     const wrappedMessage = '```' + message + '```'
 
     if (process.env.NODE_ENV !== 'production') {
       return console.log({ text: wrappedMessage })
     }
 
-    return axios.post(this.webHookURL, { text: wrappedMessage })
+    await axios.post(this.webHookURL, { text: wrappedMessage })
   }
 }
