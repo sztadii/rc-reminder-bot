ee7386432ffe66710da0355f53b8392dd2622070
Krystian Sztadhaus <sztadii@gmail.com>
2021-01-08 21:18:57 +0400

    Rename env validation logs

--- env-file-verification.js
+++ env-file-verification.js
@@ -5,7 +5,8 @@ function envFileVerification() {
   if (!fs.existsSync('.env')) {
     console.error('-------')
     console.error('-------')
-    console.error('Please rename ".env.example" to ".env" and fill with correct data')
+    console.error('Please create ".env" file in root folder and fill with correct data')
+    console.error('As an example please look at .env-example')
     console.error('-------')
     console.error('-------')
     process.exit(1)
