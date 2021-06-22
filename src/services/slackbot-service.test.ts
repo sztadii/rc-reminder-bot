6713a57d0caa38d81a9b87227427a548e5fbb4e0
Krystian Sztadhaus <sztadii@gmail.com>
2021-06-22 23:12:18 +0200

    Add missing tests

--- src/services/slackbot-service.test.ts
+++ src/services/slackbot-service.test.ts
@@ -1,9 +1,36 @@
 import SlackBotService from './slackbot-service'
+import axios from 'axios'
 
 describe('SlackBotService', () => {
+  beforeAll(() => {
+    jest.mock('axios')
+  })
+
+  afterAll(jest.resetAllMocks)
+
   it('throw an error if webHookURL is empty', () => {
     expect(() => {
       new SlackBotService('')
     }).toThrow('webHookURL is empty :(')
+  })
+
+  it('make http call when isProduction flag is true', () => {
+    const httpHandler = jest.fn()
+    axios.post = httpHandler
+
+    const slackBotService = new SlackBotService('wp.pl', true)
+    slackBotService.postMessageToReminderChannel('Some message')
+
+    expect(httpHandler).toBeCalled()
+  })
+
+  it('do not make http call when isProduction flag is false', () => {
+    const httpHandler = jest.fn()
+    axios.post = httpHandler
+
+    const slackBotService = new SlackBotService('wp.pl', false)
+    slackBotService.postMessageToReminderChannel('Some message')
+
+    expect(httpHandler).not.toBeCalled()
   })
 })
