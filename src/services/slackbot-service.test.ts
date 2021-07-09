870069cc89bcc42e83a47ec9519b9aa97db2c4e8
Krystian Sztadhaus <sztadii@gmail.com>
2021-07-09 14:12:06 +0200

    Improve DI in SlackBotService

--- src/services/slackbot-service.test.ts
+++ src/services/slackbot-service.test.ts
@@ -1,46 +1,27 @@
 import SlackBotService from './slackbot-service'
-import axios from 'axios'
 
 describe('SlackBotService', () => {
-  beforeAll(() => {
-    jest.mock('axios')
-  })
-
-  afterAll(jest.resetAllMocks)
-
   it('throw an error if webHookURL is empty', () => {
     expect(() => {
       new SlackBotService('')
     }).toThrow('webHookURL is empty :(')
   })
 
-  it('postMessageToReminderChannel will make http call by default', () => {
-    const httpHandler = jest.fn()
-    axios.post = httpHandler
-
-    const slackBotService = new SlackBotService('wp.pl')
-    slackBotService.postMessageToReminderChannel('Some message')
-
-    expect(httpHandler).not.toBeCalled()
-  })
-
   it('when isProduction flag is true, then postMessageToReminderChannel will make http call', () => {
-    const httpHandler = jest.fn()
-    axios.post = httpHandler
+    const httpPostFunc = jest.fn()
 
-    const slackBotService = new SlackBotService('wp.pl', true)
+    const slackBotService = new SlackBotService('wp.pl', true, httpPostFunc)
     slackBotService.postMessageToReminderChannel('Some message')
 
-    expect(httpHandler).toBeCalledTimes(1)
+    expect(httpPostFunc).toBeCalledTimes(1)
   })
 
   it('when isProduction flag is false, then postMessageToReminderChannel will not make http call', () => {
-    const httpHandler = jest.fn()
-    axios.post = httpHandler
+    const httpPostFunc = jest.fn()
 
-    const slackBotService = new SlackBotService('wp.pl', false)
+    const slackBotService = new SlackBotService('wp.pl', false, httpPostFunc)
     slackBotService.postMessageToReminderChannel('Some message')
 
-    expect(httpHandler).not.toBeCalled()
+    expect(httpPostFunc).not.toBeCalled()
   })
 })
